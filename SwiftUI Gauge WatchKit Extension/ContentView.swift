fcfe0e04b3dbc580571a97fc0e5e59972fba294d
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 19:21:04 +0800

    Initial Commit

---
+++ SwiftUI Gauge WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Gauge WatchKit Extension
+//
+//  Created by Amben on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Gauge WatchKit Extension/SwiftUI_GaugeApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUI_GaugeApp.swift
+//  SwiftUI Gauge WatchKit Extension
+//
+//  Created by Amben on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
